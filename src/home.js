9ee10ed66f9149bacc8494c8a4c6be13c0edb791
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-19 16:28:55 +0530

    removed brand name

--- src/home.js
+++ src/home.js
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import { getBrandName } from "./utils";
 
 const Home = () => {
-  const [brandName, setBrandName]=useState(getBrandName())
- 
+  
   return (
     <div>
       <h2>Home Pag for RDMS Application</h2>
-      <h4>BrandName : {brandName}</h4>
     </div>
   );
 };
