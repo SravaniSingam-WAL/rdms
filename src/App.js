8dcaa9d471c091fdf27c26c1f2243f31932180e0
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-31 18:42:30 +0530

    Added header for all pages except login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
-import { getToken } from "./utils";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import Login from './login';
 import Home from './home';
-import { createBrowserHistory } from 'history';
+import { getToken } from "./utils";
+import WithOutNav from "./withOutNav";
+import WithNav from "./withNav";
 
 const history = createBrowserHistory({ basename: '/rdms' });
 
@@ -13,8 +16,13 @@ function App() {
   return (
     <Router history={history} basename="/rdms">
     <Routes>
+    <Route element ={<WithOutNav />}>
     <Route path="/" element={token ? <Navigate to="/home" /> : <Login />} />
-      <Route path="/home" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+  </Route>
+  <Route element = {<WithNav />}>
+    <Route path="/home" element={<Home />} />
+    </Route>
     </Routes>
   </Router>
   );
--- src/home.js
+++ src/home.js
@@ -6,7 +6,6 @@ const Home = () => {
  
   return (
     <div>
-    <span className="brandName">{brandName}</span>
       <p className="para">Home Page for RDMS Application</p>
     </div>
   );
